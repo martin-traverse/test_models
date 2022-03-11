c111f5d3e029e9119b527f75a0c9ee172083c2d1
Greg Wiltshire <80156020+greg-wiltshire@users.noreply.github.com>
2022-03-11 10:47:01 +0000

    Update bond_pricing.py

--- bond_pricing.py
+++ bond_pricing.py
@@ -100,10 +100,10 @@ class BondPricingModel(trac.TracModel):
         #for i in range(maximum_payments_left_across_whole_portfolio):
             
         # Discount coupon payments by yield to maturity
-        bond_portfolio_valuation["PRESENT_VALUE_OF_PAYMENTS"] = bond_portfolio_valuation["PRESENT_VALUE_OF_PAYMENTS"] + (bond_portfolio_valuation["PAYMENT_PER_PERIOD"] / pow(1.015, i+1))
+        bond_portfolio_valuation["PRESENT_VALUE_OF_PAYMENTS"] = bond_portfolio_valuation["PRESENT_VALUE_OF_PAYMENTS"] + (bond_portfolio_valuation["PAYMENT_PER_PERIOD"] / pow(1.015, 0+1))
             
         # Discount face value by yield to maturity
-        bond_portfolio_valuation["PRESENT_VALUE_OF_FACE_VALUE"] = bond_portfolio_valuation["PRESENT_VALUE_OF_FACE_VALUE"] + (bond_portfolio_valuation["FACE_VALUE"] / pow(1.015, i+1))
+        bond_portfolio_valuation["PRESENT_VALUE_OF_FACE_VALUE"] = bond_portfolio_valuation["PRESENT_VALUE_OF_FACE_VALUE"] + (bond_portfolio_valuation["FACE_VALUE"] / pow(1.015, 0+1))
         
         # Sum both discounted values as full value
         bond_portfolio_valuation["BOND_VALUATION"] = bond_portfolio_valuation["PRESENT_VALUE_OF_PAYMENTS"] + bond_portfolio_valuation["PRESENT_VALUE_OF_FACE_VALUE"]
