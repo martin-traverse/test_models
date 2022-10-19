28c447b6721cdf34c8f5d8d796cc021c663c12b7
greg.wiltshire <greg.wiltshire@accenture.com>
2022-10-19 15:51:37 +0100

    Added in time dimension

--- src/impairment/calculate_mi.py
+++ src/impairment/calculate_mi.py
@@ -35,9 +35,12 @@ class CalculateMI(trac.TracModel):
     def run_model(self, ctx: trac.TracContext):
         impairment_forecast = ctx.get_pandas_table("impairment_forecast")
 
-        impairment_mi = (impairment_forecast.groupby(['business_line', "product_line", "region", "subregion", "mortgage_type"], as_index=False)
+        group_by_list = ['business_line', "product_line", "region", "subregion", "mortgage_type", "date"]
+        impairment_mi = (impairment_forecast.groupby(group_by_list, as_index=False)
               .agg({'balance': 'sum', 'ead': 'sum', 'pd_12m': 'mean', 'pd_lifetime': 'mean', 'lgd_12m': 'mean', 'lgd_lifetime': 'mean', 'ecl_12m': 'sum', 'ecl_lifetime': 'sum'})
               .rename(columns={'balance': 'total_balance', 'ead': 'total_ead', 'pd_12m': 'mean_pd_12m', 'pd_lifetime': 'mean_pd_lifetime', 'lgd_12m': 'mean_lgd_12m', 'lgd_lifetime': 'mean_lgd_lifetime', 'ecl_12m': 'total_ecl_12m', 'ecl_lifetime': 'total_ecl_lifetime'}))
+
+        impairment_mi = impairment_mi.sort_values(by=group_by_list, ascending=True)
 
         # Output the dataset
         ctx.put_pandas_table("impairment_mi", impairment_mi)
