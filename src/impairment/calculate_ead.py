c16adb0a2016714aeafa04c07c8136941d658586
greg.wiltshire <greg.wiltshire@accenture.com>
2022-10-21 12:06:25 +0100

    Added amortization to balances Updated schemas to £

--- src/impairment/calculate_ead.py
+++ src/impairment/calculate_ead.py
@@ -63,14 +63,16 @@ class CalculateEad(trac.TracModel):
         mortgage_book_t0 = ctx.get_pandas_table("mortgage_book_t0")
 
         ead_forecast = mortgage_book_t0.drop("date", axis=1).join(dates, how="cross")
-        print(ead_forecast)
+
         ead_forecast["time_to_default"] = 3 - ead_forecast["months_in_arrears"]
+
+        ead_forecast["month_index"] = 1 + (ead_forecast["date"].dt.year - first_forecast_month.year) * 12 + ead_forecast["date"].dt.month - first_forecast_month.month
+        ead_forecast["balance"] = ead_forecast["balance"] - ead_forecast["month_index"] * ead_forecast["monthly_repayment"]
+
         ead_forecast["ead"] = ead_forecast["balance"] + 400 + ead_forecast["balance"] * (
                     pow(1 + (0.05 / 12), ead_forecast["time_to_default"]) - 1)
 
-        print(ead_forecast)
-
-        ead_forecast.drop(["pd_12m", "in_default"], axis=1, inplace=True)
+        ead_forecast.drop(["pd_12m", "in_default", "month_index"], axis=1, inplace=True)
         # Output the dataset
         ctx.put_pandas_table("ead_forecast", ead_forecast)
 
